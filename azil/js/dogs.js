6146e3def178f5b607bdc1c36b61b84c424bbf15
Blodhead <81102425+Blodhead@users.noreply.github.com>
2022-06-28 18:12:48 +0200

    Animals finished

--- azil/js/dogs.js
+++ azil/js/dogs.js
@@ -2,7 +2,7 @@
 var dogs_array;
 
 function initialize(){
-  localStorage.removeItem("birds"); //delete before presentation
+  localStorage.removeItem("dogs"); //delete before presentation
     let dogs = localStorage.getItem("dogs");
     if(dogs != null){
         dogs_array = JSON.parse(dogs);
