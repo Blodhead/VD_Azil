9c2f8a0557143f4500347a743459dcc8b2498eb5
Blodhead <81102425+Blodhead@users.noreply.github.com>
2022-07-03 00:37:18 +0200

    user

--- azil/js/LogIn.js
+++ azil/js/LogIn.js
@@ -1,16 +1,41 @@
+var users;
+function initialize(){
+    var val = localStorage.getItem("users");
+    if(val != null){users = JSON.parse(val); return;}
+    users=[
+        {
+            username: "Milos",
+            password:"123456"
+        },
+        {
+            username: "Maja",
+            password:"123456"
+        }
+    ];
+    localStorage.setItem("users",JSON.stringify(users));
+
+}
+
 function checkUser(){
 
     var name = (document.getElementById("floatingInputUsername")).value;
     var pass = (document.getElementById("floatingPassword")).value;
-
-    if(localStorage.getItem(""+name) == null) {
-        alert("No such user!"); 
-        return;
-    }else if(localStorage.getItem(""+name) != pass){
-        alert("Wrong password for selected user!");
-        return;
+ 
+    var name_pass = false;
+    var check = -1;
+    var pass_pass = false;
+    for(var i = 0; i < users.length; i++){
+        if(users[i].username == name){name_pass = true; check = i; break;}
     }
-    localStorage.setItem("current_user", name);
-    alert("Welcome "+name+"!");
+    if(name_pass == false){alert("No such user!"); return;}
 
+    if(users[check].password == pass){pass_pass = true;}
+    
+
+    if(name_pass && pass_pass){
+        localStorage.setItem("current_user", name);
+        alert("Welcome "+name+"!");
+    }else{
+        alert("No such user!");
+    }
 }
